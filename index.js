3d7d50fec670223c81852c786f6ffc52fb311f45
Patrick Bowen <phunanon@users.noreply.github.com>
2022-02-07 11:07:20 +0000

    Fetch main repo README for website example

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  const req = await fetch(
-    `README.md`,
-  );
+  const req = await fetch(`https://phunanon.github.io/Insitux/README.md`);
   const text = await req.text();
   const examples = text
     .replace(/[\s\w\W]+## Various examples/, "")
